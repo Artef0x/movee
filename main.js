85f60983bc68c8705f55fde52b1b9e1e711afda3
Artef0x <tema103lis@gmail.com>
2025-10-13 14:25:19 +0300

    get burger

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const dropAction = document.querySelector('.order__payment_pay')
 const dropDown = document.querySelector('.order__check-row')
 const arrowRotate = document.querySelector('.order__arrow')
+const burger = document.querySelector('.header__burgermb')
 
 dropAction.onclick = function () {
   dropDown.classList.toggle('close')
   arrowRotate.classList.toggle('rotate')
+}
+
+burger.onclick = function () {
+  burger.classList.toggle('active')
 }
